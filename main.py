dc4e753330f0949b86e2073c7cc59989037297b7
mcj5246 <mcj5246@psu.edu>
2020-09-05 04:11:46 +0000

    Final

--- main.py
+++ main.py
@@ -3,6 +3,16 @@
 gradepoint1 = input("Enter your course 1 letter grade: ")
 credit1 = input("Enter your course 1 credit: ")
 
+A = 4.0
+A_min = 3.67
+B_plus = 3.33
+B = 3.0
+B_min = 2.67
+C_plus = 2.33
+C = 2.0
+D = 1.0
+F = 0.0
+
 if gradepoint1 == "A":
   print(f"Grade point for course 1 is: 4.0")
 elif gradepoint1 == "A-":
@@ -74,6 +84,14 @@ elif gradepoint3 == "F":
 else:
     print(f"0.0")
 
+gradepoint1 = int(gradepoint1)
+gradepoint2 = int(gradepoint2)
+gradepoint3 = int(gradepoint3)
+
+credit1 = int(credit1)
+credit2 = int(credit2)
+credit3 = int(credit3)
 
 GPA = (gradepoint1 * credit1 + gradepoint2 * credit2 + gradepoint3 * credit3) / (credit1 + credit2 + credit3)
+
 print(f"Your GPA is: {GPA}")
